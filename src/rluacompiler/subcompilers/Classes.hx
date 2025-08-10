02d31ea314e132f92fa5e508fbab4a17d7a1e496
Davvex87 <dvvex87@gmail.com>
2025-08-10 15:09:01 +0100

    Interfaces

--- src/rluacompiler/subcompilers/Classes.hx
+++ src/rluacompiler/subcompilers/Classes.hx
@@ -1,19 +1,16 @@
 package rluacompiler.subcompilers;
 
 #if (macro || rlua_runtime)
-
-import haxe.macro.Type;
-
-// Import Reflaxe types
 import reflaxe.DirectToStringCompiler;
 import reflaxe.data.ClassFuncData;
 import reflaxe.data.ClassVarData;
 import reflaxe.data.EnumOptionData;
+import haxe.macro.Type;
 import Lambda;
 
-class Classes extends SubCompiler {
-
-	public function compileClassImpl(classType: ClassType, varFields: Array<ClassVarData>, funcFields: Array<ClassFuncData>): Null<String>
+class Classes extends SubCompiler
+{
+	public function compileClassImpl(classType:ClassType, varFields:Array<ClassVarData>, funcFields:Array<ClassFuncData>):Null<String>
 	{
 		if (varFields.length == 0 && funcFields.length == 0)
 			return null;
@@ -29,6 +26,8 @@ class Classes extends SubCompiler {
 		output += '})\n';
 		output += '${classType.name}.__index = ${classType.name}\n';
 
+		output += '${classType.name}.__name__ = "${classType.name}"\n';
+
 		if (superClassName != null)
 			output += '${classType.name}.__super__ = ${superClassName}\n';
 
@@ -37,8 +36,41 @@ class Classes extends SubCompiler {
 
 		var hasInstField = Lambda.exists(varFields, v -> !v.isStatic) || Lambda.exists(funcFields, v -> !v.isStatic);
 
-		if (!classType.isInterface)
+		if (classType.isInterface)
 		{
+			var fields = varFields.map((i) -> !i.isStatic ? '"${i.field.name}"' : null)
+				.concat(funcFields.map((i) -> !i.isStatic ? '"${i.field.name}"' : null))
+				.filter((s) -> s != null);
+			output += '${classType.name}.__interface__ = {${fields.join(", ")}}\n';
+		}
+		else
+		{
+			var statics = varFields.map((i) -> i.isStatic ? '"${i.field.name}"' : null)
+				.concat(funcFields.map((i) -> i.isStatic ? '"${i.field.name}"' : null))
+				.filter((s) -> s != null);
+			var fields = varFields.map((i) -> !i.isStatic ? '"${i.field.name}"' : null)
+				.concat(funcFields.map((i) -> !i.isStatic ? '"${i.field.name}"' : null))
+				.filter((s) -> s != null);
+			if (statics.length > 0)
+				output += '${classType.name}.__fields__ = {${statics.join(", ")}}\n';
+			if (fields.length > 0)
+				output += '${classType.name}.__prototype__ = {${fields.join(", ")}}\n';
+
+			for (f in varFields)
+			{
+				if (f.getter != null || f.setter != null)
+				{
+					trace(f.getter.name);
+					trace(f.setter.name);
+				}
+			}
+
+			var properties = varFields.map((i) -> i.getter != null ? i.getter.name : null)
+				.concat(varFields.map((i) -> i.setter != null ? i.setter.name : null))
+				.filter((s) -> s != null);
+			if (properties.length > 0)
+				output += '${classType.name}.__properties__ = {${properties.join(", ")}}\n';
+
 			for (varf in varFields)
 			{
 				if (varf.isStatic)
@@ -48,7 +80,7 @@ class Classes extends SubCompiler {
 			if (hasInstField)
 			{
 				output += 'function ${classType.name}.new(...)\n';
-				output += '\tlocal self = setmetatable({__tostring = function(self) return "${classType.name}" end}, ${classType.name})\n\tself:__constructor(...)\n\treturn self\n';
+				output += '\tlocal self = setmetatable({__tostring = function(self) return "${classType.name}" end; __class__ = ${classType.name}}, ${classType.name})\n\tself:__constructor(...)\n\treturn self\n';
 				output += 'end\n';
 			}
 
@@ -63,5 +95,4 @@ class Classes extends SubCompiler {
 		return output;
 	}
 }
-
 #end
