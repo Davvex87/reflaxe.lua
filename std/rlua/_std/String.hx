a5ceab99f61a147a49c6912614b972e0f633d150
Davvex87 <102032123+Davvex87@users.noreply.github.com>
2025-09-18 09:01:15 +0100

    String class fix
    forgot to check for null values, was causing errors due to improper null handling

--- std/rlua/_std/String.hx
+++ std/rlua/_std/String.hx
@@ -2,7 +2,8 @@ package;
 
 import rlua.LuaStringTools;
 
-extern class String {
+extern class String
+{
 	var length(get, never):Int;
 
 	@:nativeFunctionCode("#{this}")
@@ -18,10 +19,10 @@ extern class String {
 	function toLowerCase():String;
 
 	inline function charAt(index:Int):String
-		return untyped string.sub(this, index+1, index+1);
+		return untyped string.sub(this, index + 1, index + 1);
 
 	inline function charCodeAt(index:Int):Null<Int>
-		return untyped #if lua_utf8 utf8.codepoint(this, index+1) #else string.byte(this, index+1) #end;
+		return untyped #if lua_utf8 utf8.codepoint(this, index + 1) #else string.byte(this, index + 1) #end;
 
 	inline function indexOf(str:String, ?startIndex:Int):Int
 		return untyped __lua__("string.find({0}, {1}, ({2} or 0)+1, true) - 1 or -1", this, str, startIndex);
@@ -32,10 +33,10 @@ extern class String {
 	inline function split(delimiter:String):Array<String>
 		return LuaStringTools.split(this, delimiter);
 
-	@:nativeFunctionCode("string.sub({this}, {arg0} + 1, {arg0} + {arg1})")
+	@:nativeFunctionCode("string.sub({this}, {arg0} + 1, {arg0} + ({arg1} or (#{this} - {arg0} + 1)))")
 	function substr(pos:Int, ?len:Int):String;
 
-	@:nativeFunctionCode("string.sub({this}, {arg0} + 1, {arg1})")
+	@:nativeFunctionCode("string.sub({this}, ({arg0} or 0) + 1, ({arg1} or 1))")
 	function substring(startIndex:Int, ?endIndex:Int):String;
 
 	@:nativeFunctionCode("tostring({this})")
